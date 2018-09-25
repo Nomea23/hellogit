543196ed7f4ab5e2bda97682b2b5caf9ab635ded
Nomea23 <lnshakoane@gmail.com>
2018-09-25 11:36:38 +0200

    - created a project with a class

---
+++ HelloMiddleEarth/src/nom/nom/HelloMiddleEarth.java
@@ -0,0 +1,18 @@
+package nom.nom;
+
+public class HelloMiddleEarth
+{
+	public static void main(String[] args) {
+		//this is my second project, will try and do as I did previously with my 2nd project
+		String[] names = {"Hello", "Bobby", "Ukhphi", "Mfo"};
+		
+		//print greetings:
+		for(int i = 0; i < names.length; i++)
+		{
+			System.out.println("Hello names: " + i);
+		}
+	}
+	
+	
+
+}
