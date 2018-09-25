9c366faa682a9fd68d256e7dc9f70ece15369030
Nomea23 <lnshakoane@gmail.com>
2018-09-25 12:34:46 +0200

    adding some comments after the conflict resolution

--- HelloMiddleEarth/src/nom/nom/HelloMiddleEarth.java
+++ HelloMiddleEarth/src/nom/nom/HelloMiddleEarth.java
@@ -12,11 +12,19 @@ public class HelloMiddleEarth
 			System.out.println("Hello names: " + i);
 		}
 	}
-<<<<<<< HEAD
+
 	//I am in eclipse and want to see if I can push this comment to github concurrently with the change made in git hub
-=======
+
 	// I am adding a comment in Git and want to see if I can pull this change in eclipse
 	
-
->>>>>>> branch 'master' of https://github.com/Nomea23/hellogit.git
+	//1. I committed the second statement/comment from Git, the
+	//2. I came to eclipse to add a comment(1st statement), then I attempted to commit and push
+	//3.committing was successful but the local master was not pushed to the origin(remote)
+	//4.Therefore I had to pull from my class in eclipse , then resolve the conflict manually
+	//5.In git staging, migrate the class to the staged changes then commit and push( merges the local and branches)
+	//NB:
+		//1. Always pull before working on anything
+		//2. commit then push
+		//3.resolve conflicts manually
+		//4. in git staging- drag from unstaged to staged changes then commit and push
 }
