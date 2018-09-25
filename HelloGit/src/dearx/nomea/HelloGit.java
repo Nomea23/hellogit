0e17fb9b3451756d2e2fa1c05d9b7134257126af
Nomea23 <lshakoane@dearx.co.za>
2018-09-25 10:27:06 +0200

    added a comment

--- HelloGit/src/dearx/nomea/HelloGit.java
+++ HelloGit/src/dearx/nomea/HelloGit.java
@@ -3,6 +3,7 @@ package dearx.nomea;
 public class HelloGit
 {
 	public static void main(String[] args) {
+		//first comment and lets see what happens!
 		System.out.println("Hello git world!!!");
 	}
 }
